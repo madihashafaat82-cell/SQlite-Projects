5778cca904c0a40b9c8d4babdd01880761e0e729
Rayyan <Rayyanshafqat@gmail.com>
2026-05-24 00:25:47 +0500

    initial

---
+++ Rating list.sql
@@ -0,0 +1,18 @@
+CREATE TABLE rates (
+Restaurant TEXT,
+Rates INTEGER
+);
+
+INSERT INTO rates (Restaurant,Rates) VALUES
+('KFC',3/5),
+('McDonalds',2/5),
+('Chunken cheese',4.5/5),
+('Ranchers',4/5),
+('Saudi Broast',4.5/5),
+('White castle',5/5),
+('CP Five star',5/5),
+('Jhonny & Jugnu',4/5),
+('Besteiro',3/5),
+('Papa Johns',1/5);
+
+SELECT*FROM rates;
